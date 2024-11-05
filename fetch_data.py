ed216a2b89b3fae36d5b57d5ef3c0546bf83171f
quakoff <36509187+quakoff@users.noreply.github.com>
2024-11-05 11:23:35 +0100

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,4 @@
-import requests
+import pandas as pd
 import logging
 
 # Konfiguracja loggera
@@ -6,22 +6,15 @@ logging.basicConfig(filename='logs.txt', level=logging.INFO, format='%(asctime)s
 logging.getLogger().addHandler(logging.StreamHandler())
 
 # Link do pliku CSV
-csv_url = pd.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/AER/CollegeDistance.csv")
+csv_url = "https://vincentarelbundock.github.io/Rdatasets/csv/AER/CollegeDistance.csv"
 
 try:
-    # Pobieranie danych bezpośrednio z URL
-    logging.info("Rozpoczęto pobieranie danych z URL.")
-    response = requests.get(csv_url)
+    # Pobieranie danych bezpośrednio do DataFrame przy użyciu Pandas
+    logging.info("Rozpoczęto pobieranie danych z URL przy użyciu Pandas.")
+    data = pd.read_csv(csv_url)
+    logging.info("Dane zostały pomyślnie załadowane do DataFrame.")
 
-    # Sprawdzamy, czy pobieranie zakończyło się sukcesem
-    if response.status_code == 200:
-        # Zapisanie pobranego pliku na lokalny dysk
-        with open('data_from_url_BACKUP.csv', 'wb') as f:
-            f.write(response.content)
-        logging.info("Dane zostały pobrane i zapisane do pliku CSV.")
-    else:
-        logging.error(f"Nie udało się pobrać danych. Kod błędu: {response.status_code}")
-        raise ValueError(f"Nie udało się pobrać danych. Kod błędu: {response.status_code}")
-
+    # Wyświetlenie kilku pierwszych wierszy jako potwierdzenie
+    print(data.head())
 except Exception as e:
     logging.error(f"Wystąpił błąd podczas pobierania danych z URL: {e}")
